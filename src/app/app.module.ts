8f611918e278485b81d93d451b88ba89dd125bdf
Jorge Pego <jorge.pego@vtxrm.com>
2023-10-19 19:41:42 +0100

    organization of app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,23 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 import { environment } from '../environments/environment';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-// import { provideAuth, getAuth } from '@angular/fire/auth';
-// import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { CoreModule } from './../../../core/src/public-api';
 
-// import { MatCardModule } from '@angular/material/card';
-// import { MatBadgeModule } from '@angular/material/badge';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatIconModule } from '@angular/material/icon';
-// import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-// import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ShopComponent } from './components/shop/shop.component';
 import { NavMainComponent } from './components/nav-main/nav-main.component';
 import { ShopListComponent } from './components/shop-list/shop-list.component';
 import { ShopItemComponent } from './components/shop-item/shop-item.component';
-
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { CoreModule } from './../../../core/src/public-api';
 import { CategorizedPipe } from './pipes/categorized.pipe';
 
 @NgModule({
@@ -39,14 +30,6 @@ import { CategorizedPipe } from './pipes/categorized.pipe';
     ShopListComponent,
     ShopItemComponent,
     CategorizedPipe,
-    // CentsToEuroPipe,
-    // AquiredPipe,
-    // JhpCardComponent,
-    // LongPressDirective,
-    // LongTouchDirective,
-    // JhpToolbarComponent,
-    // JhpLanguageSwitcherComponent,
-    // JhpBottomSheetComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,29 +44,21 @@ import { CategorizedPipe } from './pipes/categorized.pipe';
         deps: [HttpClient],
       },
     }),
-    MatSidenavModule,
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideAuth(() => getAuth()),
-    // provideFirestore(() => getFirestore()),
-
-    CoreModule,
+    CoreModule.forRoot({
+      firebaseConf: environment.firebase,
+    }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-// required for AOT compilation
-// export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-//   return new TranslateHttpLoader(http, );
-
-// }
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, environment.baseRef + 'assets/i18n/');
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     appId: '1:111919944545:web:206cf821a2894f8a7feb20',
     databaseURL: 'https://myhome-7bab1.firebaseio.com',
     storageBucket: 'myhome-7bab1.appspot.com',
-    locationId: 'us-central',
+    // locationId: 'us-central',
     apiKey: 'AIzaSyBqu6BpYkJw3O6s15W6fM2_VqjFMTHY1QU',
     authDomain: 'myhome-7bab1.firebaseapp.com',
     messagingSenderId: '111919944545',
